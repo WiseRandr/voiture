03d2e5685a6c0fe51e8abcb46f7c545e5d6bedf8
wiserandr <randriamialisonwise@gmail.com>
2021-07-21 09:18:50 +0300

    fix

--- packages/app/src/layout/menu/menu.tsx
+++ packages/app/src/layout/menu/menu.tsx
@@ -2,15 +2,19 @@ import { useMemo } from "react"
 import { useLocation } from "react-router-dom";
 import CarSVG from "src/assets/svg/car.svg";
 import LoginSVG from "src/assets/svg/login.svg";
+import { useAuth } from "src/context/auth.context";
 import { MenuIcon, MenuLink, MenuText } from "./menu.style";
 
 export default function Menu() {
   const location = useLocation();
-  const menus = useMemo(() => [
-    { href: '/', icon: <CarSVG />, text: 'Voitures' },
-    { href: '/se-connecter', icon: <LoginSVG />, text: 'Se connecter' },
-    { href: '/s-inscrire', icon: <LoginSVG />, text: `S'inscrire` },
-  ], []);
+  const { isconnected } = useAuth();
+  const menus = useMemo(() => {
+    const data = [{ href: '/', icon: <CarSVG />, text: 'Voitures' }]
+    if(!isconnected) {
+      data.push({ href: '/se-connecter', icon: <LoginSVG />, text: 'Se connecter' }, { href: '/s-inscrire', icon: <LoginSVG />, text: `S'inscrire` });
+    }
+    return data;
+}, [isconnected]);
   
   return <div className="mt-5 mx-1 text-center">
     {menus.map(menu => (
